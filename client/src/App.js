a4f8a43dc8641ec81844ea7aaf9e35ee981225d6
Edgar Chilin <edgar3ac@gmail.com>
2020-09-22 12:39:07 -0700

    added a css link to app.js. button are green and have hovering feature

--- client/src/App.js
+++ client/src/App.js
@@ -20,11 +20,17 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+         <script>  var link = document.createElement('link');
+         link.rel = 'stylesheet';  
+        link.type = 'text/css'; 
+           link.href = 'dog-finder-main/client/src/index.css'; 
+         </script>
+
           <h1 className="App-title">Hello world</h1>
-          <button onClick={() => this.buttonPress(1)}>Test endpoint 1</button>
-          <button onClick={() => this.buttonPress(2)}>Test endpoint 2</button>
-	        <button onClick={() => this.buttonPress(3)}>Test endpoint 3</button>
-          <button onClick={() => this.buttonPress(4)}>Test endpoint 4</button>
+          <button class="button1" onClick={() => this.buttonPress(1)}>Test endpoint 1</button>
+          <button class="button1" onClick={() => this.buttonPress(2)}>Test endpoint 2</button>
+	        <button class="button1" onClick={() => this.buttonPress(3)}>Test endpoint 3</button>
+          <button class="button1" onClick={() => this.buttonPress(4)}>Test endpoint 4</button>
           <p>{this.state.message}</p>
         </header>
       </div>
