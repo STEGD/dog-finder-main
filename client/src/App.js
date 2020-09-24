5090a73953e0631ae47cf482871b656ecd0d5289
Grant <geposner@cpp.edu>
2020-09-23 19:00:19 -0700

    Adding link-check in right place

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
+const linkCheck = require('link-check');
 
 class App extends Component {
   constructor(props) {
     super(props)
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
       message: ""
     }
   }
 
-  buttonPress = endpointNum => {
-    fetch(`/test${endpointNum}`)
-    .then(res => res.json())
-    .then(data => this.setState({
-      message: data.message
-    }));
+  buttonPress = endpointNum => { fetch(`/${endpointNum}`).then(res => res.json()).then(data => this.setState({ message: data.message })); }
+  
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A URL was submitted: ' + this.state.value);
+    linkCheck(this.state.value, function (err, result) {
+        if (err) {
+            alert(err);
+            return;
+        }
+        alert(`${result.link} is ${result.status}`);
+    });
+    event.preventDefault();
   }
 
   render() {
@@ -27,11 +41,18 @@ class App extends Component {
          </script>
 
           <h1 className="App-title">Hello world</h1>
-          <button class="button1" onClick={() => this.buttonPress(1)}>Test endpoint 1</button>
-          <button class="button1" onClick={() => this.buttonPress(2)}>Test endpoint 2</button>
-	        <button class="button1" onClick={() => this.buttonPress(3)}>Test endpoint 3</button>
-          <button class="button1" onClick={() => this.buttonPress(4)}>Test endpoint 4</button>
+          <button class="button1" onClick={() => this.buttonPress("test1")}>Test endpoint 1</button>
+          <button class="button1" onClick={() => this.buttonPress("test2")}>Test endpoint 2</button>
+	  <button class="button1" onClick={() => this.buttonPress("test3")}>Test endpoint 3</button>
+          <button class="button1" onClick={() => this.buttonPress("test4")}>Test endpoint 4</button>
           <p>{this.state.message}</p>
+	  <form onSubmit={this.handleSubmit}>
+  	  <label>
+  	    Url to scrape:
+  	    <input type="text" value={this.state.value} onChange={this.handleChange} />
+ 	  </label>
+ 	  <input type="submit" value="Submit" />
+	  </form>
         </header>
       </div>
     );
