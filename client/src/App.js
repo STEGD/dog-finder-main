a07da52c1f1f3202f3a959e80e5118811d2326a7
JTokos <jqtok0829@gmail.com>
2020-10-13 11:51:06 -0700

    updated navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React, {useRef, useEffect, useState} from 'react';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import PetApp from './pages/PetApp';
+import quizApp from './pages/quizApp';
+import HomeApp from './pages/HomeApp';
 import Navbar from './pages/Navbar';
 
 export default function App() {
   return (
     <div>
       <Navbar />
+      <Route exact path="/homeapp" component={HomeApp} />
+      <Route exact path="/quizapp" component={PetApp} />
+      <Route exact path="/petapp" component={quizApp} />
     </div>
   )
 }
