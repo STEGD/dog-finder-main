95d464b5118125906f898a74eb7353c7bb8e23a5
Daniel Santana <Santana.danny4g@gmail.com>
2020-10-18 21:51:28 -0700

    Minor modification & adjustments

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,8 @@ export default function App() {
     <div>
       <Navbar />
       <Route exact path="/homeapp" component={HomeApp} />
-      <Route exact path="/quizapp" component={PetApp} />
-      <Route exact path="/petapp" component={quizApp} />
+      <Route exact path="/quizapp" component={quizApp} />
+      <Route exact path="/petapp" component={PetApp} />
       <Footer/>
     </div>
   )
