0d7bfe8f03bc2737b4cb2a6b332a8191abfe53d4
Daniel Santana <Santana.danny4g@gmail.com>
2020-10-19 18:30:37 -0700

    Adjusting homepage

--- client/src/pages/HomeApp.js
+++ client/src/pages/HomeApp.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './HomeApp.css'
+
 
 export default function HomeApp() {
+
     return (
         <div>
-            <p>This is the home page of Pet Finder.</p>
+            <div className = 'top-container'>
+            <header className = 'landing-info'>
+            <div className= 'landing-contents'>
+            <h1 className= 'landing-info-text'>Welcome to the pet finder!</h1>
+            <Link to= '/quizapp' className='landing-quiz-btn'>Take Quiz</Link>
+            </div>
+            </header>
+            </div>
+            <div className="about-section">
+                About us
+            </div>
+            <div className="pet-display">
+                Meet, API call to pets
+            </div>
+
         </div>
     )
 }
