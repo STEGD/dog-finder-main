37367d30e1483700b7c1b0c6a6251a60648a3a80
Edgar Chilin <edgar3ac@gmail.com>
2020-10-25 14:19:33 -0700

    added an alert for button

--- client/src/pages/QuizComponents/ButtonSubmit.js
+++ client/src/pages/QuizComponents/ButtonSubmit.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import CardLayout from './CardLayout'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -11,30 +12,30 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 
-function dothis(){
-    alert
+function dothis( Q1, Q2, Q3, Q4) {
+    if(Q1 !== "" || Q2 !== "" || Q3 !== "" || Q4 !== "" ){
+        alert("hello world\n" + Q1 + "\n" + Q3 + "\n" +Q2 + "\n" + Q4 )
+    }else{
+        alert("Please answer all the question")
+    }
 }
-
-function dothis(){
-   // alert("Your answers are being verified")
-}
-
 
 export default function ButtonSubmit(props) {
     
 
     const classes = useStyles();
+    let q1 = `${props.question1Response}`
+    let q2 = `${props.question2Response}`
+    let q3 = `${props.question3Response}`
+    let q4 = `${props.question4Response}`
     return (
         <div>
-            <Button variant ="contained" color="primary" onClick={dothis()}>
-                {props.text}
-              
+            <Button variant ="contained" color="primary" onClick={()=>dothis(q1, q2, q3, q4)}>
+                {props.text}   
             </Button>
-            <h1>{props.question1Response}</h1>
-               <h1>{props.question2Response}</h1>
-               <h1>{props.question3Response}</h1>
-               <h1>{props.question4Response}</h1>
-               <h1>{props.question5Response}</h1>
+           
         </div>
     )
 }
+
+
