9516449295ea4eb56b5c0a600f9731196e1a87d4
Daniel Santana <Santana.danny4g@gmail.com>
2020-10-26 00:54:49 -0700

    Added switch to direct to homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import PetApp from './pages/PetApp';
 import quizApp from './pages/quizApp';
 import HomeApp from './pages/HomeApp';
@@ -13,9 +13,14 @@ export default function App() {
   return (
     <div>
       <Navbar />
+      <Switch>
       <Route exact path="/homeapp" component={HomeApp} />
       <Route exact path="/quizapp" component={quizApp} />
       <Route exact path="/petapp" component={PetApp} />
+
+      <Route exact path = '/' component={HomeApp}/>
+
+      </Switch>
       <Footer/>
     </div>
   )
