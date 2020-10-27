6ee251d52089f45416021e0247877609cb157cea
Edgar Chilin <edgar3ac@gmail.com>
2020-10-27 10:15:07 -0700

    survey

---
+++ client/src/pages/QuizComponents/CardArray.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import CardLayout from './CardLayout'
+import './CardArray.css'
+
+export default function CardArray() {
+    return (
+        <div className="grid-container-q">
+        <div className="grid-container-cardarray">
+            <CardLayout />
+            <CardLayout />
+            <CardLayout />
+            <CardLayout />
+            <CardLayout />
+            <CardLayout />
+            <CardLayout />
+            <CardLayout />
+
+        </div>
+        </div>
+    )
+}
--- client/src/pages/QuizComponents/RadioQuestion.js
+++ client/src/pages/QuizComponents/RadioQuestion.js
@@ -7,9 +7,7 @@ export default function RadioQuestion(props) {
 
     const handleChange=(e)=>{
        setResponse(e.target.value)
-       console.log(response)
        props.onChange(e.target.value)
-       console.log("done")
     }
     return (
         <div className="grid-container">
