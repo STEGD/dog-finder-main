a2865dbf8d840d77f49e3c0c735282309d40896f
Edgar Chilin <edgar3ac@gmail.com>
2020-11-01 20:53:32 -0800

    survey response answer output update

--- client/src/pages/QuizComponents/RadioQuestion.js
+++ client/src/pages/QuizComponents/RadioQuestion.js
@@ -18,13 +18,30 @@ export default function RadioQuestion(props) {
     displayC = setDisplay(displayC);
     displayD = setDisplay(displayD);
 
+    let A = `${props.AnswerA}`
+    let B = `${props.AnswerB}`
+    let C = `${props.AnswerC}`
+    let D = `${props.AnswerD}`
+
+
     const [showDisplayC, setShowDisplayC] = useState(displayC)
     const [showDisplayD, setShowDisplayD] = useState(displayD)
 
 
     const handleChange=(e)=>{
        setResponse(e.target.value)
-       props.onChange(e.target.value)
+       let answers = "";
+       let choice = e.target.value;
+        if(choice === "A"){
+            answers = A;
+        }else if(choice === "B"){
+            answers = B;
+        }else if(choice === "C"){
+            answers = C;
+        }else if(choice === "D"){
+            answers = D;
+        }
+       props.onChange(answers)
     }
     return (
         <div className="grid-container">
@@ -38,7 +55,7 @@ export default function RadioQuestion(props) {
                 color ="primary"
                 onChange={handleChange}
                 />
-                <p>{props.AnswerA}</p>
+                <p>{A}</p>
             </div>
             <div className="responseB">
                 <Radio value="B" 
@@ -47,7 +64,7 @@ export default function RadioQuestion(props) {
                 onChange={handleChange}
 
                 />
-                <p>{props.AnswerB}</p>
+                <p>{B}</p>
             </div>
         { showDisplayC ?
             <div className="responseC">
@@ -57,7 +74,7 @@ export default function RadioQuestion(props) {
                 color ="secondary"
                 onChange={handleChange}
                 />
-                <p>{props.AnswerC}</p>
+                <p>{C}</p>
             </div>
         :null}
         { showDisplayD ?
@@ -67,7 +84,7 @@ export default function RadioQuestion(props) {
                 color ="secondary"
                 onChange={handleChange}
                 />
-                <p>{props.AnswerD}</p>
+                <p>{D}</p>
             </div>
             : null }
         </div>
