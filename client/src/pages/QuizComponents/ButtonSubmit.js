b6e0aeedaafd0a69a3a646e8a45e5daba1857932
Edgar Chilin <edgar3ac@gmail.com>
2020-11-11 21:08:42 -0800

    quizCard displays data, added temporary pet card

--- client/src/pages/QuizComponents/ButtonSubmit.js
+++ client/src/pages/QuizComponents/ButtonSubmit.js
@@ -1,8 +1,7 @@
-import React, {useStates} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import CardLayout from './CardLayout'
-import CardArray from './CardArray'
+import QuizCardLayout from './QuizCardLayout'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -16,43 +15,8 @@ const useStyles = makeStyles((theme) => ({
 export default function ButtonSubmit(props) {
 
     const [showResults, setShowResults] = React.useState(false);
-
-
-    function dothis( Q1, Q2, Q3, Q4, Q5,Q6,Q7,Q8,Q9,Q10) {
-        let baseURL =  'https://us-central1-dog-finder-fae9d.cloudfunctions.net/app';
-        if(Q1 !== "" && Q2 !== "" && Q3 !== "" && Q4 !== "" && Q5 !== "" && Q6 !== ""&& Q7 !== "" && Q8 !== "" && Q9 !== "" && Q10 !== "") {
-            const dataObj = {
-                "costPerYear": parseInt(Q1),
-                "firstYearCost": parseInt(Q2),
-                "personalActivityLevel": parseInt(Q3),
-                "petActivityLevel": parseInt(Q4),
-                "petSize": parseInt(Q5),
-                "kidsUnderTen": Q6 == 1 ? true : false,    // 1 = Yes
-                "kidsOverTen": Q7 == 1 ? true: false,      // 1 = Yes
-                "allergies": Q8 == 1 ? true : false,       // 1 = Yes
-                "freeTime": parseInt(Q9),
-                "petExperience": parseInt(Q10)
-            };
-
-            const reqOptions = {
-                method: 'POST',
-                headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-                body: JSON.stringify({ 'data': dataObj })
-            }
-
-            fetch(`${baseURL}/api/submit`, reqOptions)
-                .then(res => res.json())
-                .then(data => console.log(data))
-
-            // setShowResults(true) 
-            
-        } else {
-            alert("Please answer all the question");
-        }
-      
-      }
-
-    const classes = useStyles();
+    const [petInfo, setPetInfo] = useState([]);
+    const [inputOptions, setInputOptions] = useState(false)
     let q1 = `${props.question1Response}`;
     let q2 = `${props.question2Response}`;
     let q3 = `${props.question3Response}`;
@@ -64,17 +28,78 @@ export default function ButtonSubmit(props) {
     let q9 = `${props.question9Response}`;
     let q10 = `${props.question10Response}`;
 
-      
+    useEffect( () =>{
+        if(inputOptions){
+            getData();
+        }
+        setInputOptions(false);
+      },[inputOptions]);
+    
+
+    
+    function getData(){
+            console.log("valid Entry")
+            let baseURL =  'https://us-central1-dog-finder-fae9d.cloudfunctions.net/app';
+            const dataObj = {
+                "costPerYear": parseInt(q1),
+                "firstYearCost": parseInt(q2),
+                "personalActivityLevel": parseInt(q3),
+                "petActivityLevel": parseInt(q4),
+                "petSize": parseInt(q5),
+                "kidsUnderTen": q6 == 1 ? true : false,    // 1 = Yes
+                "kidsOverTen": q7 == 1 ? true: false,      // 1 = Yes
+                "allergies": q8 == 1 ? true : false,       // 1 = Yes
+                "freeTime": parseInt(q9),
+                "petExperience": parseInt(q10)
+            };
+
+            const reqOptions = {
+                method: 'POST',
+                headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+                body: JSON.stringify({ 'data': dataObj })
+            }
+
+            fetch(`${baseURL}/api/submit`, reqOptions)
+                .then(res => res.json())
+                .then(data => setPetInfo(data.value))
+    }
+        
+    function dothis( Q1, Q2, Q3, Q4, Q5,Q6,Q7,Q8,Q9,Q10) {
+        if(q1 !== "" && q2 !== "" && q3 !== "" && q4 !== "" && q5 !== "" 
+        && q6 !== ""&& q7 !== "" && q8 !== "" && q9 !== "" && q10 !== "") {
+            setInputOptions(true)
+            //getData()
+            
+            setShowResults(true) 
+        } else {
+            alert("Please answer all the question");
+        }
+      }
+
+      function updateINT_Text(value){
+        let textValue = ""+(value).toString();
+        return textValue;
+    }
+
+    const classes = useStyles();
+
     return (
         <div>
             <Button variant ="contained" color="primary"
                  onClick={()=>{dothis(q1, q2, q3, q4, q5, q6, q7, q8, q9, q10)}}>
                 {props.text}   
             </Button>
-            { showResults ? <CardArray />: null }
+            
+
+        {showResults ? 
+        <div className="card-display-submit">
+            {Object.keys(petInfo).map(pet =>{
+            return   <QuizCardLayout key ={pet} details={petInfo[pet]} />
+            }
+            )}
+        </div>
+        : null}
            
         </div>
     )
 }
-
-
